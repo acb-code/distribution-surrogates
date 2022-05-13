f4c938093f353170efb537eab18695a9a347dd43
acb-code <69542078+acb-code@users.noreply.github.com>
2022-05-12 17:03:46 -0700

    Set up plotting for nb 4 and now doing final test, also moved objects to separate file

--- emulator_classes.py
+++ emulator_classes.py
@@ -13,6 +13,11 @@ import numpy as np
 from scipy import stats as st
 from sklearn.preprocessing import StandardScaler
 from doepy import build
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+from matplotlib.offsetbox import AnchoredText
 
 # custom functions
 import stats_functions as sf
@@ -62,14 +67,14 @@ class Simulation:
         """set the values of the input parameters for the simulation"""
         input_params = self.input_vals
         param_list = [[input_params[4] * input_params[7], 3.0], [input_params[5] * 0.8, 10.0],
-                      [input_params[0],100.0], [-350.0,input_params[5]+10.*input_params[6]],
-                      [input_params[11]+0.3,0.3], [0.1*input_params[0], 1.2*input_params[1]],
+                      [input_params[0], 100.0], [-350.0, input_params[5]+10.*input_params[6]],
+                      [input_params[11]+0.3, 0.3], [0.1*input_params[0], 1.2*input_params[1]],
                       [input_params[-2], 3.0], [35000.0, input_params[1]], [input_params[-3]*4., 3.9],
                       [-1.5, 1.0, 1.5, 1.0, 1.0 - input_params[4], input_params[4]],
                       [input_params[2], 1.0, 5, 3.0, 0.5, 0.5],
                       [55.0*input_params[4], 5.0, 85.0, 5.0, input_params[3], 1.0 - input_params[3]],
-                      [input_params[4]**2, 0.1, 0.9, 0.25, 0.5, 0.5], [input_params[4],1.0],
-                      [input_params[-5]*0.5,0.6], [35.0,input_params[5]], [3.0*input_params[6]-1.5,3.1],
+                      [input_params[4]**2, 0.1, 0.9, 0.25, 0.5, 0.5], [input_params[4], 1.0],
+                      [input_params[-5]*0.5, 0.6], [35.0, input_params[5]], [3.0*input_params[6]-1.5, 3.1],
                       [input_params[6], 0.5, -1.0, 1.0], [5.0, 1.0*input_params[6], -1.0, 1.0],
                       [2.0, input_params[7], -1.0, 1.0], [input_params[7], 5.0, -1.0, 1.0],
                       [input_params[8], 2.0], [1.0*input_params[6], 2.0], [1.5, 3.0*input_params[6]],
@@ -172,11 +177,10 @@ class Data:
     -------
 
     """
-    def __init__(self, samples=None, ecdf_vals=None, custom_bins=None, custom_scaler=None, discrete_flags=None):
+    def __init__(self, samples=None, ecdf_vals=None, custom_bins=None, scaler=None, discrete_flags=None):
         self.samples = samples
         self.custom_bins = custom_bins
-        self.custom_scaler = custom_scaler
-        self.scaler = None
+        self.scaler = scaler
         self.scaled_samples = None
         self.scaled_epdfs = None
         self.scaled_ecdfs = ecdf_vals
@@ -196,10 +200,9 @@ class Data:
 
     def scale_samples(self):
         """Scale samples based on custom scaler input or else sklearn StandardScaler - requires samples set up"""
-        if self.custom_scaler is None:
+        if self.scaler is None:
             # use new sklearn standard scaler
             self.scaler = StandardScaler()
-            self.custom_scaler = self.scaler
             # format to scale across full parametric dataset (scale to the full range)
             samples_formatted_for_scaler = cpgen.format_data_for_global_scaler(self.samples)
             self.scaler.fit(samples_formatted_for_scaler)
@@ -208,7 +211,6 @@ class Data:
             self.scaled_samples = cpgen.reshape_data_after_scaling(scaled_samples_in_scaler_format,
                                                                    self.samples.shape[0])
         else:
-            self.scaler = self.custom_scaler
             samples_formatted_for_scaler = cpgen.format_data_for_global_scaler(self.samples)
             scaled_samples_in_scaler_format = self.scaler.transform(samples_formatted_for_scaler)
             self.scaled_samples = cpgen.reshape_data_after_scaling(scaled_samples_in_scaler_format,
@@ -217,7 +219,7 @@ class Data:
     def descale_samples(self):
         """Descale the scaled_samples and save"""
         if self.scaler is None:
-            self.scaler = self.custom_scaler
+            print("Check that scaler set up correctly")
         scaled_samples_reshaped_for_scaler = cpgen.format_data_for_global_scaler(self.scaled_samples)
         descaled_samples_in_scaler_shape = self.scaler.inverse_transform(scaled_samples_reshaped_for_scaler)
         self.samples = cpgen.reshape_data_after_scaling(descaled_samples_in_scaler_shape,
@@ -225,7 +227,7 @@ class Data:
 
     def get_scaled_ecdfs_from_samples(self):
         """Transform scaled samples into ecdf format - requires scaled samples set up"""
-        ecdf_y = sf.get_ecdf_y(self.scaled_samples[0,:,0])
+        ecdf_y = sf.get_ecdf_y(self.scaled_samples[0, :, 0])
         ecdf_x = np.apply_along_axis(sf.get_ecdf_x, axis=1, arr=self.scaled_samples)
         self.scaled_ecdfs = (ecdf_y, ecdf_x)
 
@@ -238,24 +240,19 @@ class Data:
             epdf_probs = np.apply_along_axis(sf.get_epdf_probs, arr=self.scaled_samples, bins=num_bins, axis=1)
         else:
             # bins are given
-            bins_to_use = self.custom_bins
-            epdf_bins = np.zeros_like(self.scaled_samples)
-            epdf_probs = np.zeros_like(self.scaled_samples)
+            epdf_bins = self.custom_bins
+            epdf_probs = np.zeros((epdf_bins.shape[0], (self.custom_bins.shape[1]-1), epdf_bins.shape[2]))
             for i in range(epdf_bins.shape[0]):
                 for j in range(epdf_bins.shape[2]):
-                    bins = bins_to_use[i, :, j]
-                    epdf_bins[i, :, j] = bins
-                    epdf_probs = sf.get_epdf_probs(arr=self.scaled_samples[i, :, j], bins=bins)
+                    bins = epdf_bins[i, :, j]
+                    epdf_probs[i, :, j] = sf.get_epdf_probs(arr=self.scaled_samples[i, :, j], bins=bins)
         self.scaled_epdfs = (epdf_bins, epdf_probs)
 
     def scale_3darray(self, arr, scale_flag=True):
         """Descale a 3d array of data - wrapper for scaler object that's set up for scaling across all input points"""
         step1_stack = cpgen.format_data_for_global_scaler(arr)
         if self.scaler is None:
-            if self.custom_scaler is not None:
-                self.scaler = self.custom_scaler
-            else:
-                print("Error with scaling")
+            print("Error with scaling")
         if scale_flag:
             step2 = self.scaler.transform(step1_stack)
         else:
@@ -294,8 +291,8 @@ class Data:
     def sample_input_ecdfs(self):
         """Fill out scaled samples attribute based on sampling scaled ecdf attribute"""
         local_samples = np.apply_along_axis(sf.sample_ecdf, arr=self.scaled_ecdfs[1],
-                                                  axis=1, num_samples=self.scaled_ecdfs[1].shape[1],
-                                                  ecdfy=self.scaled_ecdfs[0])
+                                            axis=1, num_samples=self.scaled_ecdfs[1].shape[1],
+                                            ecdfy=self.scaled_ecdfs[0])
         self.scaled_samples = local_samples
         self.samples = self.scale_3darray(local_samples, scale_flag=False)
 
@@ -375,14 +372,129 @@ class Experiment:
                 self.data = Data(samples=self.dataset, discrete_flags=self.discrete_flags)
             else:
                 self.data = Data(samples=self.dataset, discrete_flags=self.discrete_flags,
-                                 custom_scaler=self.custom_scaler)
+                                 scaler=self.custom_scaler)
         else:
             if self.custom_scaler is None:
                 self.data = Data(samples=self.dataset, discrete_flags=self.discrete_flags, custom_bins=self.custom_bins)
             else:
                 self.data = Data(samples=self.dataset, discrete_flags=self.discrete_flags, custom_bins=self.custom_bins,
-                                 custom_scaler=self.custom_scaler)
+                                 scaler=self.custom_scaler)
         self.data.data_setup_from_samples()
+
+
+def calc_ks(true_sample_arr, approx_sample_arr):
+    """Calculate the KS metric - based on input 3d arrays of samples"""
+    ks_metric_holder = np.ones((true_sample_arr.shape[0], 1, true_sample_arr.shape[2]))
+    for i in range(true_sample_arr.shape[0]):
+        for j in range(true_sample_arr.shape[2]):
+            ks_m, ks_p = st.ks_2samp(true_sample_arr[i, :, j], approx_sample_arr[i, :, j])
+            ks_metric_holder[i, 0, j] = ks_m
+    return ks_metric_holder
+
+
+def calc_js(true_epdf_arr, approx_epdf_arr):
+    """Calculate the JS metric - based on input 3d arrays of epdf probs with same bins"""
+    js_metric_holder = np.ones((true_epdf_arr.shape[0], 1, true_epdf_arr.shape[2]))
+    for i in range(true_epdf_arr.shape[0]):
+        for j in range(true_epdf_arr.shape[2]):
+            js_d = sf.compute_js_dist(true_epdf_arr[i, :, j], approx_epdf_arr[i, :, j])
+            js_metric_holder[i, 0, j] = js_d
+    return js_metric_holder
+
+
+def get_initial_diff_df(arr, test_type_label='ks', cross_val_label='train', comparison_label='model'):
+    """Convert a 3d array into a df based on input labels"""
+    df = pd.DataFrame(arr.ravel(order='F').reshape((arr.shape[2], arr.shape[0])).T.flatten(), columns=['metric'])
+    df['test_type'] = test_type_label
+    df['cross_val'] = cross_val_label
+    df['comparison'] = comparison_label
+    return df
+
+
+class Analysis:
+    """
+    Object to Data classes and use them to generate plots and goodness-of-fit metrics, and also
+    include a function to generate data to drive an interactive dash plot
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+
+    """
+    def __init__(self, true_data=None, comparison_data=None, model_data=None, cross_val_type=None):
+        self.true_data = true_data
+        self.comparison_data = comparison_data
+        self.model_data = model_data
+        self.model_ks = None
+        self.model_js = None
+        self.comparison_ks = None
+        self.comparison_js = None
+        self.differencing_df = None
+        self.cross_val_type = cross_val_type
+        self.percent_reasonable_model_ks = None
+        self.percent_reasonable_model_js = None
+
+    def calc_differencing_metrics(self):
+        """Calculate the KS or JS differencing metrics based comparing Data objects"""
+        # start_time = dt.datetime.now()
+        self.model_ks = calc_ks(self.true_data.scaled_samples, self.model_data.scaled_samples)
+        self.model_js = calc_js(self.true_data.scaled_epdfs[1], self.model_data.scaled_epdfs[1])
+        self.comparison_ks = calc_ks(self.true_data.scaled_samples, self.comparison_data.scaled_samples)
+        self.comparison_js = calc_js(self.true_data.scaled_epdfs[1], self.comparison_data.scaled_epdfs[1])
+        # elapsed_time = dt.datetime.now() - start_time
+        # print("Time to calculate differencing metrics: ", elapsed_time)
+
+    def get_differencing_df(self):
+        """Combine differencing metrics into a pd.DataFrame for connecting to plotting"""
+        dfs_to_combine = [get_initial_diff_df(self.comparison_ks, test_type_label='ks',
+                                              cross_val_label=self.cross_val_type, comparison_label='true'),
+                          get_initial_diff_df(self.comparison_js, test_type_label='js',
+                                              cross_val_label=self.cross_val_type, comparison_label='true'),
+                          get_initial_diff_df(self.model_ks, test_type_label='ks',
+                                              cross_val_label=self.cross_val_type, comparison_label='model'),
+                          get_initial_diff_df(self.model_js, test_type_label='js',
+                                              cross_val_label=self.cross_val_type, comparison_label='model')]
+        self.differencing_df = pd.concat(dfs_to_combine, axis=0)
+        return self.differencing_df
+
+    def get_percent_reasonable(self):
+        """Calculate the percentage of model data that is at least as close as the farthest true data for
+        each test type"""
+        benchmark_ks = np.nanmax(self.comparison_ks)
+        benchmark_js = np.nanmax(self.comparison_js)
+
+        model_outliers_locations_ks = np.where(self.model_ks < benchmark_ks)
+        model_outliers_locations_js = np.where(self.model_js < benchmark_js)
+        num_model_outliers_ks = len(self.model_ks[model_outliers_locations_ks])
+        num_model_outliers_js = len(self.model_js[model_outliers_locations_js])
+        self.percent_reasonable_model_ks = 100. * (num_model_outliers_ks / self.model_ks.size)
+        self.percent_reasonable_model_js = 100. * (num_model_outliers_js / self.model_js.size)
+
+    def plot_single_stat_consistency_comparison(self):
+        """Plot the comparison of statistical consistency for KS and JS for one crossval type"""
+        # main violin plots
+        g = sns.catplot(data=self.differencing_df, x='test_type', y='metric', hue='comparison', palette='Set3',
+                        height=8, aspect=1.2, inner='quartile', col='cross_val', kind='violin')
+        (g.set_axis_labels("", "")
+          .set_xticklabels(["Kolmogorov-Smirnov\nmetric", "Jensen-Shannon\ndistance"], size=17)
+          .set(ylim=(0,1))
+          ._legend.remove())
+        plt.legend(fontsize=18)
+        # add ks metric results annotation
+        ks_anno = "{:.0f}".format(self.percent_reasonable_model_ks) +\
+                  '% of modeled distributions\nas close as farthest true'
+        at_ks = AnchoredText(ks_anno, prop=dict(size=14), frameon=True, loc='center left')
+        at_ks.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        g.axes[0][0].add_artist(at_ks)
+        # add js metric results annotation
+        js_anno = "{:.0f}".format(self.percent_reasonable_model_js) +\
+                  '% of modeled distributions\nas close as farthest true'
+        at_js = AnchoredText(js_anno, prop=dict(size=14), frameon=True, loc='center right')
+        at_js.patch.set_boxstyle("round,pad=0.,rounding_size=0.2")
+        g.axes[0][0].add_artist(at_js)
+        return g
 
 
 
